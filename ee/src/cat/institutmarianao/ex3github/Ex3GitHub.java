ad386c0f4ac12830a0977f76e2f02c575a68837f
ernestperezb <ernestperezb@gmail.com>
2022-10-25 19:27:04 +0200

    El ejercicio 3

---
+++ ee/src/cat/institutmarianao/ex3github/Ex3GitHub.java
@@ -0,0 +1,9 @@
+package cat.institutmarianao.ex3github;
+
+public class Ex3GitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Hola como estas angel soy ernest el que te apuñala.");
+	}
+
+}
