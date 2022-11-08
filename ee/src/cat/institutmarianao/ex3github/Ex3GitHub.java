138a98defdf00167fd97921442a2d3a6028bee55
angelacosta03 <acosta.angel.2206@gmail.com>
2022-11-08 19:21:18 +0100

    Si lees esto apruebas

--- ee/src/cat/institutmarianao/ex3github/Ex3GitHub.java
+++ ee/src/cat/institutmarianao/ex3github/Ex3GitHub.java
@@ -4,6 +4,6 @@ public class Ex3GitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hola como estas angel soy ernest el que te apuñala.");
+		System.out.println("Hola ernest el que te va a apuñalar soy yo peazo fracasaolooo.");
 	}
-
 }
